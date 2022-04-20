f118c14aae39044adffc5f9c48830195aa71531b
Arkodeep Koley <1ms20is022@msrit.edu>
2022-04-21 00:16:24 +0530

    Drum

---
+++ modify.js
@@ -0,0 +1,55 @@
+//Adding an eevent listener
+//The process is as follows: addEventListener("click",a function is called);
+
+// document.querySelector("button").addEventListener("click", handleClicks);
+
+// function handleClicks() {
+//     alert("I got clicked!!");
+// }
+
+//The above part can also be done by this following:
+for (var i = 0; i < document.querySelectorAll(" .drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        this.style.color = "blue";
+
+    });
+}
+
+//Her i am clicking on the second button
+
+//Now we setup each sound for a different button::
+
+document.getElementsByClassName("w")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("a")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("s")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("d")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("j")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("k")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+});
+
+document.getElementsByClassName("l")[0].addEventListener("click", function() {
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+});
